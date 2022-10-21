1d94037566212f70516a0418bcc7bf379f09c491
Merijn <prim@pr1mula.nl>
2022-10-21 13:01:13 +0100

    fixed buttons resume

--- resume/resume.js
+++ resume/resume.js
@@ -1,10 +1,22 @@
-const home = document.getElementById('homeCSS');
+const home = document.getElementById('homeClass');
 const homebtn = document.getElementById('homeMenu');
+const test = document.getElementById('testClass');
+const testbtn = document.getElementById('testMenu');
 
 homebtn.addEventListener('click', function handleClick(){
     if (home.style.display === 'none'){
         home.style.display = 'flex';
+        test.style.display = 'none';
     } else {
         home.style.display = 'none';
+    }
+});
+
+testbtn.addEventListener('click', function handleClick(){
+    if (test.style.display === 'none'){
+        test.style.display = 'flex';
+        home.style.display = 'none';
+    } else {
+        test.style.display = 'none';
     }
 });
